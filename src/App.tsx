d715afe11d52610fc73b8a6322a6e66932562ec5
Antonio <tonyqm19@gmail.com>
2025-03-13 22:28:57 -0500

    mi primer cambio

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import './App.css'
 
 function App() { //--nombre : string
   const nombre_1 = "Antonio Quezada";
-  //const nombre_2 = "Zuly";
+  const nombre_2 = "Zuly";
 
   return (
     <>
       <h3>Hola Amigo 1 : {nombre_1} </h3>
+      <h2>Hola Amigo 1 : {nombre_2} </h2>
     </>
   )
 }
