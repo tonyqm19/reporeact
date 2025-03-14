33b73b622469322b7e289e5ce8c41c35aadf2de4
Antonio <tonyqm19@gmail.com>
2025-03-13 21:58:53 -0500

    first commit, creacion del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() { //--nombre : string
+  const nombre_1 = "Antonio Quezada";
+  //const nombre_2 = "Zuly";
+
+  return (
+    <>
+      <h3>Hola Amigo 1 : {nombre_1} </h3>
+    </>
+  )
+}
+
+
+export default App
